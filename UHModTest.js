4b8a177ed1436534068240cb8882496211132b8e
GamingDimiGD <gamingdimigd@gmail.com>
2024-08-21 20:49:09 +0300

    bugfix 修霸葛

--- UHModTest.js
+++ UHModTest.js
@@ -1,9 +1,8 @@
-const MOD_INFO = {
+MOD_DATA.push({
     title: '測試模組',
     author: '遊戲迪米',
     description: '測試模組功能的東西，建議測試完刪掉，沒有用👍'
-}
-MOD_DATA.push(MOD_INFO)
+})
 
 console.log('success? i think so...')
 console.log('oh wait i think im supposed to say hello world')
