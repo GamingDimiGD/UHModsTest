94f9b16b126228b17e0b55cf5ff1944f3ae79154
GamingDimiGD <gamingdimigd@gmail.com>
2024-08-21 19:12:52 +0300

    呃

--- UHModTest.js
+++ UHModTest.js
@@ -1,4 +1,9 @@
-TITLE = 'TEST'
+MOD_INFO = {
+    title: '測試模組',
+    author: '遊戲迪米'
+}
+
+
 console.log('success? i think so...')
 console.log('oh wait i think im supposed to say hello world')
 console.log('hello world!')
